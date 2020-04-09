73a70db91e592468b03fac839506510e4d025a27
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-09 12:41:53 -0500

    Create Intro.py

---
+++ codeacademy-python3/strings/Intro.py
@@ -0,0 +1,15 @@
+#Like any other list, each character in a string has an index.
+
+my_name = 'Saul'
+first_initial = my_name[0]
+
+#Not only can we select a single character from a string, we can select entire chunks of characters from a string. 
+#Slices
+first_name = "Rodrigo"
+last_name = "Villanueva"
+
+# Slicing the first five letters of his last_name.
+new_account = last_name[:5]
+
+#slice out of the third through sixth letters of his last_name.
+temp_password = last_name [2:6]
