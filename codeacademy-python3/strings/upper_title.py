7d0a56afca8ebf6eb4d50da18d05353335caa5d0
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-09 14:52:16 -0500

    Create upper_title.py

---
+++ codeacademy-python3/strings/upper_title.py
@@ -0,0 +1,15 @@
+#.upper() returns the string with all uppercase characters.
+#.title() returns the string in title case, which means the first letter of each word is capitalized.
+
+poem_title = "spring storm"
+poem_author = "William Carlos Williams"
+
+poem_title_fixed = poem_title.title()
+
+print(poem_title)
+print(poem_title_fixed)
+
+poem_author_fixed = poem_author.upper()
+
+print(poem_author)
+print(poem_author_fixed)
