05207f8beb470143ecdfe2b4dc9fb699a0ccb475
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-09 16:29:03 -0500

    Update format_string.py

--- codeacademy-python3/strings/format_string.py
+++ codeacademy-python3/strings/format_string.py
@@ -3,3 +3,10 @@ def poem_title_card(poet, title):
   return "The poem \"{}\" is written by {}.".format(title, poet)
 
 print(poem_title_card("Walt Whitman", "I Hear America Singing"))
+
+
+def poem_description(publishing_date, author, title, original_work):
+  poem_desc = "The poem {} by {} was originally published in {} in {}.".format(title, author, original_work, publishing_date)
+  return poem_desc
+
+my_beard_description = poem_description('1974', 'Shel Silverstein', 'My Beard', 'Where the Sidewalk Ends')
