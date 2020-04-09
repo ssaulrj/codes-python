4e917f6ffeea6a9b92717c5c687f3b8dcafc5104
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-09 12:54:50 -0500

    Create negative_indices.py

---
+++ codeacademy-python3/strings/negative_indices.py
@@ -0,0 +1,7 @@
+company_motto = "Copeland's Corporate Company helps you capably cope with the constant cacophony of daily life"
+
+#Use negative indices to find the the second to last character
+second_to_last = company_motto[-2:-1]
+
+#Use negative indices to create a slice of the last 4 characters 
+final_word = company_motto[-4:]
