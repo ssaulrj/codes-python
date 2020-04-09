ff6fca681f7cc6e52278be9e8d04fcc6ba89f379
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-09 14:55:22 -0500

    Create split_string.py

---
+++ codeacademy-python3/strings/split_string.py
@@ -0,0 +1,5 @@
+#.split() is performed on a string, takes one argument, and returns a list of substrings found between the given argument (which in the case of .split() is known as the delimiter). 
+line_one = "The sky has given over"
+line_one_words = line_one.split()
+
+print(line_one_words)
