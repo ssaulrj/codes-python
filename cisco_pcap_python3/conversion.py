cbb2b9c38784ab5514d0895fb7adec736be7a3ea
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-09 21:56:04 -0500

    Update conversion.py

--- cisco_pcap_python3/conversion.py
+++ cisco_pcap_python3/conversion.py
@@ -6,3 +6,19 @@ ateto_a = float(input("Inserta la longitud del primer cateto: "))
 cateto_b = float(input("Inserta la longitud del segundo cateto "))
 hipo = (cateto_a**2 + cateto_b**2) ** .5
 print("La longitud de la hipotenusa es: ", hipo)
+
+# ingresa un valor flotante para la variable a aquí
+a = 12.3
+# ingresa un valor flotante para la variable b aquí
+b = 14.2
+
+# muestra el resultado de la suma aquí 
+print ('suma a+b '+str(a+b))
+# muestra el resultado de la resta aquí
+print('resta '+ str(a-b))
+# muestra el resultado de la multiplicación aquí
+print(a*b)
+# muestra el resultado de la división aquí
+print(a/b)
+
+print("\n¡Eso es todo, amigos!")
