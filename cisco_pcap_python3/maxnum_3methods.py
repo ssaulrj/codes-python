f7fe8781d169e006c7f080def2d568d130b310b1
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-09 22:31:00 -0500

    Create maxnum_3methods.py

---
+++ cisco_pcap_python3/maxnum_3methods.py
@@ -0,0 +1,48 @@
+#lee dos números
+numero1 = int (input("Ingresa el primer número:"))
+numero2 = int (input("Ingresa el segundo número:"))
+
+#elegir el número más grande
+if numero1> numero2:
+    nmasGrande = numero1
+else:
+    nmasGrande = numero2
+
+#imprimir el resultado
+print("El número más grande es:", nmasGrande)
+#--------------------------------------------------------------------
+
+#lee dos números
+numero1 = int (input("Ingresa el primer número:"))
+numero2 = int (input("Ingresa el segundo número:"))
+
+# elegir el número más grande
+if numero1 > numero2: nmasGrande = numero1
+else: nmasGrande = numero2
+
+#imprimir el resultado
+print("El número más grande es: ", nmasGrande)
+
+#--------------------------------------------------------------------
+#lee tres números
+numero1 = int (input("Ingresa el primer número:"))
+numero2 = int (input("Ingresa el segundo número:"))
+numero3 = int (input("Ingresa el tercer número:"))
+
+#asumimos temporalmente que el primer número
+#es el más grande
+#lo verificaremos pronto
+nmasGrande = numero1
+
+#comprobamos si el segundo número es más grande que el mayor número actual
+#y actualiza el número más grande si es necesario
+if numero2 > nmasGrande:
+    nmasGrande = numero2
+
+#comprobamos si el tercer número es más grande que el mayor número actual
+#y actualiza el número más grande si es necesario
+if numero3 > nmasGrande:
+    nmasGrande = numero3
+
+#imprimir el resultado
+print("El número más grande es:", nmasGrande)
