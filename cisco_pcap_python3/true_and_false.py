2461d7abd77c3f9a23ebfde86226f310a26c68b2
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-09 22:11:42 -0500

    Create true_and_false.py

---
+++ cisco_pcap_python3/true_and_false.py
@@ -0,0 +1,13 @@
+#El operador == (igual a) compara los valores de dos operandos. Si son iguales, el resultado de la comparación es True. Si no son iguales, el resultado de la comparación es False.
+var = 0 # asignando 0 a var
+print(var == 0)
+
+var = 1 # asignando 1 a var
+print(var == 0)
+
+#El operador != (no es igual a) también compara los valores de dos operandos. Aquí está la diferencia: si son iguales, el resultado de la comparación es False. Si no son iguales, el resultado de la comparación es True.
+var = 0 # asignando 0 a var
+print(var != 0)
+
+var = 1 # asignando 1 a var
+print(var != 0)
