5821ede2a9731d4b56c0a8f917d6f18333e08c41
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-09 21:43:21 -0500

    Create equation_operators.py

---
+++ cisco_pcap_python3/equation_operators.py
@@ -0,0 +1,12 @@
+#Observa el código en el editor: lee un valor flotante, lo coloca en una variable llamada x, e imprime el valor de la variable llamada y. Tu tarea es completar el código para evaluar la siguiente expresión:
+#3x3 - 2x2 + 3x - 1 
+
+# codifica aquí tus datos de prueba.
+x = 0 # y = -1.0
+x = 1 # y = 3.0
+x = -1 # y = -9.0
+x = float(x)
+# escribe tu código aquí.
+y = ((3)*(x**3)) - ((2)*(x**2)) + (3*x) - 1
+
+print("y =", y)
