7c77112de395ae77f3e5a61e813af53598a7ab83
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-09 23:19:47 -0500

    Create input_Espatifilo.py

---
+++ cisco_pcap_python3/input_Espatifilo.py
@@ -0,0 +1,11 @@
+#Espatifilo, más comúnmente conocida como la planta de Cuna de Moisés o flor de la paz, es una de las plantas para interiores más populares que filtra las toxinas dañinas del aire. Algunas de las toxinas que neutraliza incluyen benceno, formaldehído y amoníaco. 
+
+#Imagina que tu programa de computadora ama estas plantas. Cada vez que recibe una entrada en forma de la palabra Espatifilo, grita involuntariamente a la consola la siguiente cadena:  "¡Espatifilo es la mejor planta de todas!" 
+
+x = input('Enter word: ')
+if x == 'Espatifilo':
+    print('Si, ¡El Espatifilo es la mejor planta de todos los tiempos!')
+elif x == 'espatifilo':
+    print('No, ¡quiero un gran Espatifilo!')
+else:
+    print('¡Espatifilo! ¡No [{}]!'.format(x))
