131f9a8a15c05d75d5ab400221eef12dedd7dac5
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-09 21:52:32 -0500

    Create conversion.py

---
+++ cisco_pcap_python3/conversion.py
@@ -0,0 +1,8 @@
+lgo = float(input("Inserta un número: "))
+resultado = algo ** 2.0
+print(algo, "al cuadrado es", resultado)
+
+ateto_a = float(input("Inserta la longitud del primer cateto: "))
+cateto_b = float(input("Inserta la longitud del segundo cateto "))
+hipo = (cateto_a**2 + cateto_b**2) ** .5
+print("La longitud de la hipotenusa es: ", hipo)
