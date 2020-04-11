761c8b1913139450a1a306a6c45f7cf755c78f39
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-10 20:22:19 -0500

    Create append_insert_list.py

---
+++ cisco_pcap_python3/append_insert_list.py
@@ -0,0 +1,19 @@
+numeros = [111, 7, 2, 1]
+print(len(numeros))
+print(numeros)
+
+###
+
+numeros.append(4)
+
+print(len(numeros))
+print(numeros)
+
+###
+
+numeros.insert(0,222)
+print(len(numeros))
+print(numeros)
+
+#
+
