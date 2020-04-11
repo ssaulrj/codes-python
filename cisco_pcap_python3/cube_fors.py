cc5455249afe862b74f24296769f1d45ba8fac16
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-11 13:00:59 -0500

    Create cube_fors.py

---
+++ cisco_pcap_python3/cube_fors.py
@@ -0,0 +1,31 @@
+cubos = [num ** 3 for num in range (5)]
+print(cubos) # salidas: [0, 1, 8, 27, 64]
+
+# Una tabla de cuatro columnas y cuatro filas: un arreglo bidimensional (4x4)
+
+table = [[":(", ":)", ":(", ":)"],
+         [":)", ":(", ":)", ":)"],
+         [":(", ":)", ":)", ":("],
+         [":)", ":)", ":)", ":("]]
+
+print(tabla)
+print(tabla [0][0]) # salida: ':('
+print(tabla [0][3]) # salida: ':)'
+
+# Cubo - un arreglo tridimensional (3x3x3)
+
+cubo = [[[':(', 'x', 'x'],
+         [':)', 'x', 'x'],
+         [':(', 'x', 'x']],
+
+        [[':)', 'x', 'x'],
+         [':(', 'x', 'x'],
+         [':)', 'x', 'x']],
+
+        [[':(', 'x', 'x'],
+         [':)', 'x', 'x'],
+         [':)', 'x', 'x']]]
+
+print(cubo)
+print(cubo [0][0][0]) # salida: ':('
+print(cubo [2][2][0]) # salida: ':)'
