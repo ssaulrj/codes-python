1286979996bc93b83ff205e93e10ea69ff7f81ad
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-12 14:52:35 -0500

    Create dictionary.py

---
+++ codeacademy-python3/dictionary/dictionary.py
@@ -0,0 +1,15 @@
+sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+num_cameras = {"backyard": 6, "garage": 2, "driveway": 1}
+
+print(sensors)
+print(num_cameras)
+
+translations = {
+  "mountain" : "orod",
+  "bread" : "bass",
+  "friend" : "mellon",
+  "horse" : "roch",
+}
+
+#Value and dictionary
+children = {"von Trapp":["Johannes", "Rosmarie", "Eleonore"], "Corleone":["Sonny", "Fredo", "Michael"]}
