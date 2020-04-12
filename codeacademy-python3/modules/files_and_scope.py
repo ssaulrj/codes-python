6943ae26180d44760fa801d480d07755e2b79217
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-12 13:46:47 -0500

    Create files_and_scope.py

---
+++ codeacademy-python3/modules/files_and_scope.py
@@ -0,0 +1,11 @@
+#In library.py:
+# Add your always_three() function below:
+def always_three():
+  return 3
+  
+#In script.py
+# Import library below:
+from library import always_three
+
+# Call your function below:
+always_three()
