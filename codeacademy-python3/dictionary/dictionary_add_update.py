d39e55b0b94839ffd0df8ea836965d8aae6d9881
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-12 15:06:15 -0500

    Create dictionary_add_update.py

---
+++ codeacademy-python3/dictionary/dictionary_add_update.py
@@ -0,0 +1,12 @@
+animals_in_zoo = {}
+animals_in_zoo["zebras"] = 8
+animals_in_zoo["monkeys"] = 12
+animals_in_zoo["dinosaurs"] = 0
+
+print(animals_in_zoo)
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+
+user_ids.update({"theLooper":138475, "stringQueen":85739})
+
+print(user_ids)
