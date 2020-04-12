debe6de05488a4d46a0672dcd2d8b78c303ddb95
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-12 15:23:11 -0500

    Update dictionary_add_zip.py

--- codeacademy-python3/dictionary/dictionary_add_zip.py
+++ codeacademy-python3/dictionary/dictionary_add_zip.py
@@ -6,6 +6,7 @@ oscar_winners["Best Picture"] = "Moonlight"
 
 print(oscar_winners)
 
+#---------------------------------------------------------------------------------------------------------
 drinks = ["espresso", "chai", "decaf", "drip"]
 caffeine = [64, 40, 0, 120]
 
@@ -14,3 +15,5 @@ zipped_drinks = zip(drinks, caffeine)
 drinks_to_caffeine = {key:value for key, value in zipped_drinks}
 
 print(drinks_to_caffeine)
+
+
