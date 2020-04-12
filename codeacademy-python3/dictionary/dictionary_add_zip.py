649987d8f09452f8a517d5b627a99bf1358e2884
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-12 15:17:00 -0500

    Create dictionary_add_zip.py

---
+++ codeacademy-python3/dictionary/dictionary_add_zip.py
@@ -0,0 +1,16 @@
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+
+oscar_winners["Supporting Actress"] = "Viola Davis"
+
+oscar_winners["Best Picture"] = "Moonlight"
+
+print(oscar_winners)
+
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+
+zipped_drinks = zip(drinks, caffeine) 
+
+drinks_to_caffeine = {key:value for key, value in zipped_drinks}
+
+print(drinks_to_caffeine)
