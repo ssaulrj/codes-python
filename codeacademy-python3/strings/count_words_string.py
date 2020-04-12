89613b4169da759078bcac24961cc9ebcf116eb5
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-11 19:40:28 -0500

    Create count_words_string.py

---
+++ codeacademy-python3/strings/count_words_string.py
@@ -0,0 +1,9 @@
+# Write your count_multi_char_x function here:
+def count_multi_char_x(word, x):
+  num = word.count(x)
+  return num
+# Uncomment these function calls to test your function:
+print(count_multi_char_x("mississippi", "iss"))
+# should print 2
+print(count_multi_char_x("apple", "pp"))
+# should print 1
