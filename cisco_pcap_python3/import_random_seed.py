8d9656b9ba193919f35e2c2378eca4951d3a27da
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-12 20:47:29 -0500

    Create import_random_seed.py

---
+++ cisco_pcap_python3/import_random_seed.py
@@ -0,0 +1,29 @@
+from random import random
+
+for i in range(5):
+    print(random())
+
+#---------------------------------------------------------------- 
+from random import random, seed
+
+seed(0)
+
+for i in range(5):
+    print(random())
+    
+#----------------------------------------------------------------     
+from random import randrange, randint
+
+print(randrange(1), end=' ')
+print(randrange(0, 1), end=' ')
+print(randrange(0, 1, 1), end=' ')
+print(randint(0, 1))
+
+#---------------------------------------------------------------- 
+from random import choice, sample
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(choice(lst))
+print(sample(lst, 5))
+print(sample(lst, 10))
