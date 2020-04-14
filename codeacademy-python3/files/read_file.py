90a86391bb1453785dd296885e986342005fbeab
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-14 13:45:55 -0500

    Create read_file.py

---
+++ codeacademy-python3/files/read_file.py
@@ -0,0 +1,8 @@
+with open('welcome.txt') as cool_doc:
+  cool_contents = cool_doc.read()
+print(cool_contents)
+
+with open('welcome.txt') as text_file:
+  text_data = text_file.read()
+
+print(text_data)
