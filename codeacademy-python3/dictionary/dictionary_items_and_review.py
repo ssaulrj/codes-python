731e55b69fc40955a0e61b07475b21d621017429
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-13 21:16:07 -0500

    Create dictionary_items_and_review.py

---
+++ codeacademy-python3/dictionary/dictionary_items_and_review.py
@@ -0,0 +1,15 @@
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+for x,y in pct_women_in_occupation.items():
+  print("Women make up "+str(y)+" percent of "+x+"s")
+  
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+
+spread = {}
+
+spread["past"] = tarot.pop(13,"None")
+spread["present"] = tarot.pop(22,"None")
+spread["future"] = tarot.pop(10,"None")
+
+for x,y in spread.items():
+  print("Your "+str(x)+" is the "+str(y)+" card.")
