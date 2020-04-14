6850da64f32915187c1817d5bd7d9ac17a187ae0
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-14 13:16:31 -0500

    Create dictionary_word_length_dictionary.py

---
+++ codeacademy-python3/dictionary/dictionary_word_length_dictionary.py
@@ -0,0 +1,13 @@
+# Write your word_length_dictionary function here:
+def word_length_dictionary(words):
+  dic = {}
+  for x in words:
+    num = len(x)
+    dic[x] = num
+  return dic
+  
+# Uncomment these function calls to test your  function:
+print(word_length_dictionary(["apple", "dog", "cat"]))
+# should print {"apple":5, "dog": 3, "cat":3}
+print(word_length_dictionary(["a", ""]))
+# should print {"a": 1, "": 0}
