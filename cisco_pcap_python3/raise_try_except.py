f2563f681a9465087f71c92cba3b0c0a7a194f24
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-14 16:30:27 -0500

    Update raise_try_except.py

--- cisco_pcap_python3/raise_try_except.py
+++ cisco_pcap_python3/raise_try_except.py
@@ -7,3 +7,17 @@ except ArithmeticError:
     print("¿Que pasó? ¿Un error?")
 
 print("FIN.")
+
+def badFun(n):
+    try:
+        return n / 0
+    except:
+        print("¡Lo hice otra vez!")
+        raise
+
+try:
+    badFun(0)
+except ArithmeticError:
+    print("¡Ya veo!")
+
+print("FIN.")
