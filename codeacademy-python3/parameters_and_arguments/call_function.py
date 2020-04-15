fd2edb97ada2867f7d9665b0bf88ef2a666c797b
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-15 18:07:05 -0500

    Create call_function.py

---
+++ codeacademy-python3/parameters_and_arguments/call_function.py
@@ -0,0 +1,10 @@
+from record_library import place_record, rotate_record, drop_needle
+
+def play_record(album):
+  place_record(album)
+  rotate_record(album)
+  drop_needle(album)
+
+next_album = "Blondie / Parallel Lines"
+
+play_record(next_album)
