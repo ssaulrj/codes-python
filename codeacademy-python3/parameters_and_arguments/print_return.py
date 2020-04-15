ba908c06da1e354d859cbdd9e1e2767f0113a952
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-15 18:21:03 -0500

    Create print_return.py

---
+++ codeacademy-python3/parameters_and_arguments/print_return.py
@@ -0,0 +1,18 @@
+# store the result of this print() statement in the variable prints_return
+prints_return = print("What does this print function return?")
+
+# print out the value of prints_return
+print(prints_return)
+
+# call sort_this_list.sort() and save that in list_sort_return
+sort_this_list = [14, 631, 4, 51358, 50000000]
+list_sort_return = sort_this_list.sort()
+
+# print out the value of list_sort_return
+print(list_sort_return)
+
+"""
+Output:
+None
+None
+"""
