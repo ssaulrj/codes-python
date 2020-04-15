fc75b87c6c446921ec2535915e6c673eca82fbc9
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-15 15:15:36 -0500

    Create subclasses.py

---
+++ codeacademy-python3/classes/subclasses.py
@@ -0,0 +1,14 @@
+class Bin:
+  pass
+
+#Subclasses
+class RecyclingBin(Bin):
+  pass
+
+"""class User:
+  is_admin = False
+  def __init__(self, username)
+    self.username = username
+
+class Admin(User):
+  is_admin = True"""
