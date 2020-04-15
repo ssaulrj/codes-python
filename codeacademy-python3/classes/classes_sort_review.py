3f89e0533f51628ae2354cc9dd4da6bf15adda28
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-15 17:41:58 -0500

    Create classes_sort_review.py

---
+++ codeacademy-python3/classes/classes_sort_review.py
@@ -0,0 +1,11 @@
+class SortedList(list):
+  def append(self, value):
+    super().append(value)
+    self.sort()
+
+"""
+Some things to consider:
+
+-When a SortedList gets initialized with unsorted values (say if you call SortedList([4, 1, 5])) those values don’t get sorted! How would you change SortedList so that the list is sorted right after the object gets created?
+-What other Python builtins have functionality “missing”? Could you write a new dictionary that uses a fallback value when it tries to retrieve an item and can’t?
+"""
