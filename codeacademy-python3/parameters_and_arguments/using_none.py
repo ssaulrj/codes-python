51d268031f3c183ab33a3d9c045adf4a9e05a432
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-15 22:51:45 -0500

    Create using_none.py

---
+++ codeacademy-python3/parameters_and_arguments/using_none.py
@@ -0,0 +1,15 @@
+def update_order(new_item, current_order=None):
+  if current_order is None:
+    current_order = []    
+  current_order.append(new_item)
+  return current_order
+
+# First order, burger and a soda
+order1 = update_order({'item': 'burger', 'cost': '3.50'})
+order1 = update_order({'item': 'soda', 'cost': '1.50'}, order1)
+
+# Second order, just a soda
+order2 = update_order({'item': 'soda', 'cost': '1.50'})
+
+# What's in that second order again?
+print(order2)
