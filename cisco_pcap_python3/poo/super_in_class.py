93608f0d88c11919e41eeac6e123f0761e0a9fb0
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-16 18:45:56 -0500

    Create super_in_class.py

---
+++ cisco_pcap_python3/poo/super_in_class.py
@@ -0,0 +1,32 @@
+class Super:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def __str__(self):
+        return "Mi nombre es " + self.nombre + "."
+
+class Sub(Super):
+    def __init__(self, nombre):
+        Super.__init__(self, nombre)
+
+
+obj = Sub("Andy")
+
+print(obj)
+
+#.-----------------------------------------------
+class Super:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def __str__(self):
+        return "Mi nombre es " + self.nombre + "."
+
+class Sub(Super):
+    def __init__(self, nombre):
+        super().__init__(nombre)
+
+
+obj = Sub("Andy")
+
+print(obj)
