9459ef63cb1afc916eefef2edfcd50c15a2a0c7c
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-17 12:27:57 -0500

    Create readline_s_stream.py

---
+++ cisco_pcap_python3/files/readline_s_stream.py
@@ -0,0 +1,49 @@
+from os import strerror
+
+try:
+    ccnt = lcnt = 0
+    s = open('text.txt', 'rt')
+    line = s.readline()
+    while line != '':
+        lcnt += 1
+        for ch in line:
+            print(ch, end='')
+            ccnt += 1
+        line = s.readline()
+    s.close()
+    print("\n\nCaracteres en el archivo: ", ccnt)
+    print("Lineas en el archivo:     ", lcnt)
+except IOError as e:
+    print("Se produjo un error de E/S: ", strerr(e.errno))
+    
+#-----------------------------------------------------------------
+from os import strerror
+
+try:
+    ccnt = lcnt = 0
+    s = open('text.txt', 'rt')
+    lines = s.readlines(20)
+    while len(lines) != 0:
+        for line in lines:
+            lcnt += 1
+            for ch in line:
+                print(ch, end='')
+                ccnt += 1
+        lines = s.readlines(10)
+    s.close()
+    print("\n\nCaracteres en el archivo: ", ccnt)
+    print("Lineas en el archivo:     ", lcnt)
+except IOError as e:
+    print("Se produjo un error de E/S: ", strerr(e.errno))
+    
+    
+"""
+Beautiful is better than ugly.
+
+Explicit is better than implicit.
+
+Simple is better than complex.
+
+Complex is better than complicated.
+
+"""
