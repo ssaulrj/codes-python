cc8dd6c4685458a861a58643305324aafd6edb17
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-17 12:35:59 -0500

    Create write_stream.py

---
+++ cisco_pcap_python3/files/write_stream.py
@@ -0,0 +1,22 @@
+from os import strerror
+
+try:
+	fo = open('newtext.txt', 'wt') #un nuevo archivo (newtext.txt) es creado
+	for i in range(10):
+		s = "línea #" + str(i+1) + "\n"
+		for ch in s:
+			fo.write(ch)
+	fo.close()
+except IOError as e:
+	print("Se produjo un error de E/S: ", strerr(e.errno))
+  
+#-----------------------------------------------------------
+from os import strerror
+
+try:
+	fo = open('newtext.txt', 'wt')
+	for i in range(10):
+		fo.write("line #" + str(i+1) + "\n")
+	fo.close()
+except IOError as e:
+	print("Se produjo un error de E/S: ", strerr(e.errno))
