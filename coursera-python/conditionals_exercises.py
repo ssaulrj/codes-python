56fbba9fdbf5b8d2377e132111f4fc6a60001098
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-06-21 22:33:46 -0500

    Create conditionals_exercises.py

---
+++ coursera-python/conditionals_exercises.py
@@ -0,0 +1,17 @@
+def is_positive(number):
+  if number > 0:
+    return True
+  else:
+    return None
+    
+def number_group(number):
+  if number > 0:
+    return "Positive"
+  elif number == 0:
+    return "Zero"
+  else:
+    return "Negative"
+
+print(number_group(10)) #Should be Positive
+print(number_group(0)) #Should be Zero
+print(number_group(-5)) #Should be Negative
