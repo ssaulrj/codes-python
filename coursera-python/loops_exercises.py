c93e48584eb335f0b121ad7a1fceb0c828b0dd24
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-06-22 22:05:57 -0500

    Create loops_exercises.py

---
+++ coursera-python/loops_exercises.py
@@ -0,0 +1,96 @@
+def count_down(start_number):
+  current = start_number
+  while (current > 0):
+    print(current)
+    current -= 1
+  print("Zero!")
+
+count_down(3)
+
+def print_range(start, end):
+	# Loop through the numbers from start to end
+	n = start
+	while n <= end:
+		print(n)
+		n += 1
+
+print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line) 
+
+#Print_prime_factors function print all the prime factors of a number. A prime factor is a number that is prime and divides another without a remainder.
+def print_prime_factors(number):
+  # Start with two, which is the first prime
+  factor = 2
+  # Keep going until the factor is larger than the number
+  while factor <= number:
+    # Check if factor is a divisor of number
+    if number % factor == 0:
+      # If it is, print it and divide the original number
+      print(factor)
+      number = number / factor
+    else:
+      # If it's not, increment the factor by one
+      factor += 1
+  return "Done"
+
+print_prime_factors(100)
+# Should print 2,2,5,5
+# DO NOT DELETE THIS COMMENT
+
+def is_power_of_two(n):
+  if n == 0:
+    return False
+  # Check if the number can be divided by two without a remainder
+  while n % 2 == 0:
+    n = n / 2
+    # If after dividing by two the number is 1, it's a power of two
+  if n == 1:
+    return True
+  return False
+
+print(is_power_of_two(0)) # Should be False
+print(is_power_of_two(1)) # Should be True
+print(is_power_of_two(8)) # Should be True
+print(is_power_of_two(9)) # Should be False
+
+#It returns the sum of all the divisors of a number, without including it. A divisor is a number that divides into another without a remainder.
+def sum_divisors(n):
+  if n == 0:
+    return 0
+  sumvar = 0
+  # Return the sum of all divisors of n, not including n
+  for numbervar in range(1, n): 
+    if (n % numbervar) == 0:
+      sumvar += numbervar
+  return sumvar
+
+print(sum_divisors(0))
+# 0
+print(sum_divisors(3)) # Should sum of 1
+# 1
+print(sum_divisors(36)) # Should sum of 1+2+3+4+6+9+12+18
+# 55
+print(sum_divisors(102)) # Should be sum of 2+3+6+17+34+51
+# 114
+
+#The multiplication_table function prints the results of a number passed to it multiplied by 1 through 5. An additional requirement is that the result is not to exceed 25, which is done with the break statement. 
+def multiplication_table(number):
+	# Initialize the starting point of the multiplication table
+	multiplier = 1
+	# Only want to loop through 5
+	while multiplier <= 5:
+		result = number*multiplier
+		# What is the additional condition to exit out of the loop?
+		if result > 25:
+			break
+		print(str(number) + "x" + str(multiplier) + "=" + str(result))
+		# Increment the variable for the loop
+		multiplier += 1
+
+multiplication_table(3) 
+# Should print: 3x1=3 3x2=6 3x3=9 3x4=12 3x5=15
+
+multiplication_table(5) 
+# Should print: 5x1=5 5x2=10 5x3=15 5x4=20 5x5=25
+
+multiplication_table(8)	
+# Should print: 8x1=8 8x2=16 8x3=24
