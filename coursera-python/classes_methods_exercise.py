778fd7afc6914535b5569b4766c0c08104b87530
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-07-03 13:14:52 -0500

    Update classes_methods_exercise.py

--- coursera-python/classes_methods_exercise.py
+++ coursera-python/classes_methods_exercise.py
@@ -21,3 +21,13 @@ some_person = Person("Saúl")
 # Call the greeting method
 print(some_person.greeting())
 
+#Docstring to the greeting method. How about, “Outputs a message with the name of the person”.
+class Person:
+  def __init__(self, name):
+    self.name = name
+  def greeting(self):
+    """Outputs a message with the name of the person"""
+    print("Hello! My name is {name}.".format(name=self.name)) 
+
+help(Person)
+
